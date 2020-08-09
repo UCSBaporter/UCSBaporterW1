7548e78cab2b2c7fc346f929092f5fa073503032
UCSBaporter <abigailporter@ucsb.edu>
2020-08-09 02:04:08 -0700

    "Project Structure"

---
+++ Untitled.R
@@ -0,0 +1,13 @@
+fs::dir_tree("first-webpage")
+navbar
+rmarkdown::render_site(https://github.com/UCSBaporter/UCSBaporterW1.git)
+rmarkdown::render_site("first-webpage")
+## [11] "yeti"
+rmarkdown:::themes()
+rmarkdown::render_site("first-webpage")
+rmarkdown:::themes()
+rmarkdown::render_site("first-webpage")
+rmarkdown::render_site("first-webpage")
+install.packages("devtools")
+devtools::install_github("ropenscilabs/icon")
+remotes::install_github("ropenscilabs/icon")
